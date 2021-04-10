b0e607b5c22597c975ba14979e4f995c43f768c5
jani-Ahmed <aboutjani12@gmail.com>
2021-04-11 02:12:04 +0500

    Making a front Page

---
+++ main.js
@@ -0,0 +1,7 @@
+let label = document.getElementById('label');
+let input = document.getElementById("input");
+
+function ChnageStyle() {
+    label.style.display = "inline";
+    input.placeholder = "";
+}
